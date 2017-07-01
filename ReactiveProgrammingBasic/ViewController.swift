3743142c1070f64ef1d384f3b11d04a2df68fd4d
Sheldon Wang <daniel.wangxiao@gmail.com>
2017-06-30 20:33:26 -0500

    final

--- ReactiveProgrammingBasic/ViewController.swift
+++ ReactiveProgrammingBasic/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf: UITextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    var targetString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.addObserver(self, forKeyPath: "targetString", options: [.old,.new], context: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "targetString" {
+     //       print("old:",change?[NSKeyValueChangeKey.oldKey] ?? "")
+     //       print("new:",change?[NSKeyValueChangeKey.newKey] ?? "")
+            
+            resultLbl.text = self.value(forKey: "targetString") as! String
+        }
     }
-
+    
+    @IBAction func changeTapped(_ sender: Any) {
+        guard let text = tf.text else {
+            return
+        }
+        self.setValue(text, forKey: "targetString")
+    }
 
 }
 
